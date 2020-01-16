bcb3cc17ed0a13109d37d77a5f57b138602c8bbe
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-16 20:55:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(sentence)
   split_sentence = sentence.split(" ")
-  reverse_word_letters_sentece = split_sentence.map
+  reverse_word_letters_sentece = split_sentence.map{|word| word.reverse}
+  
 end
